9f3d1ea24a711760747e9eeda64b0b53cc4b1b6d
VictorMetzler <victor.metzler@ciaathletica.com.br>
2020-08-19 15:32:30 -0300

    Componente Atualizar Produto #01

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,7 +29,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -37,6 +36,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { LOCALE_ID } from '@angular/core';
 import localPt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localPt);
 
@@ -52,7 +52,7 @@ registerLocaleData(localPt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,

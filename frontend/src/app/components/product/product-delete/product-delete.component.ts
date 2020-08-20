222e8b4d0ba31225d5e76e77e5e412966ea8e5c4
VictorMetzler <victor.metzler@ciaathletica.com.br>
2020-08-20 11:01:56 -0300

    Desafio Componente Excluir Produto - Resposta 2

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -19,7 +19,7 @@ export class ProdutcDeleteComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {     
-    const id = this.route.snapshot.paramMap.get('id')
+    const id = Number(this.route.snapshot.paramMap.get('id'));
     this.productService.readById(id).subscribe(product => {
       this.product = product
     })
